4cc04279a942f0d63f2a825389200b7b6f05db0d
ojohnson1 <ojohnson659@gmail.com>
2024-01-19 10:56:22 -0500

    added script and html file and page structure

---
+++ scripts/weeklypicks.js
@@ -0,0 +1,21 @@
+"use strict"
+
+
+const redVeletBtn= getElementbyId("redVelvetBrownies");
+const carrotCakeBtn= getElementbyId("carrotCakeCookies");
+const whiteChocBtn= getElementbyId("whiteChocolateBrownies");
+const oreoBtn= getElementbyId("oreoBrownies");
+
+
+window.onload=init;
+
+
+function init(){
+       
+}
+
+
+
+
+
+
