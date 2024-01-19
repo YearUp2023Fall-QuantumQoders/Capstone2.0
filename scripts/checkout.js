8601c83decafe89632dcb7980c96903943cac2a6
KoichiMin <koichidminami@gmail.com>
2024-01-19 10:48:12 -0500

    added functionality to cart

---
+++ scripts/checkout.js
@@ -0,0 +1,29 @@
+"use strict"
+
+window.onload = () =>{
+    displayCartItems();
+}
+
+const displayCartItems = () =>{
+
+    let cartItems = JSON.parse(sessionStorage.getItem('cart')) || [];
+    let cartItemCount = document.getElementById("cartItemCount");
+    let cartItemsContainer = document.getElementById('cartItems');
+    let finalCost = document.getElementById("finalCost");
+    let total = 0;
+    cartItemsContainer.innerHTML = '';
+    cartItemCount.innerText = cartItems.length
+
+
+        cartItems.forEach(item => {
+            
+            let itemElement = document.createElement('p');
+            let priceInteger = parseInt(item.item.slice(1));
+            let costItem = priceInteger * item.amount;
+            total += costItem;
+            itemElement.innerHTML = `<a href="products.html">${item.title}</a><span> x ${item.amount}</span> <span class="price">${costItem}$</span>`;
+            cartItemsContainer.appendChild(itemElement);
+        });
+        
+        finalCost.innerText = total + "$";
+}
